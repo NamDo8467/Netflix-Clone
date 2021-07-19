c311b87acfe6790398f22a448f3c7b2d458302d2
NamDo8467 <huynhnamdo@gmail.com>
2021-07-19 19:29:53 +0700

    add routes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React from 'react'
 import Home from './Home/Home'
 import './App.css'
 import Login from './Login/Login'
+import SignUp from './SignUp/SignUp'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 function App() {
     return (
+        <Router>
+            <Route exact path='/' component={Home}/>
+            <Route path='/login' component={Login} />
+            <Route path='/signup' component={SignUp}/>
+        </Router>
         
-        // <Home />
-        <Login />
     )
 }
 
