adb8f0de91403361f7c1d4b5ecaf95eca3b643fd
NamDo8467 <huynhnamdo@gmail.com>
2021-07-21 16:05:50 +0700

    set up restrictions for database

--- server/model/User.js
+++ server/model/User.js
@@ -1,8 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const { isEmail } = require("validator");
 
 const userSchema = mongoose.Schema({
-    name:String,
-    username: String,
-    password: String,
-})
-module.exports = mongoose.model('User', userSchema)
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter your email"],
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, "Please enter a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please enter your password"],
+    minlength: [8, "Minimum password length is 8 characters"],
+  },
+});
+module.exports = mongoose.model("User", userSchema);
