b69efa74827ad3be8c9805fddc8034f88d2a8ddd
NamDo8467 <huynhnamdo@gmail.com>
2021-07-28 10:41:49 +0700

    delete comment

--- server/verifyCookie.js
+++ server/verifyCookie.js
@@ -1,20 +1,17 @@
 const jwt = require("jsonwebtoken");
-const cookieParser = require("cookie-parser");
 
 const verifyCookie = (req, res, next) => {
   const cookie = req.cookies.cookie;
   if (cookie) {
     jwt.verify(cookie, "netflix secret key", (err, result) => {
       if (err) {
-        res.status(400).send({message:"Not verified"})
+        res.status(400).send({ message: "Not verified" });
       } else {
-        // console.log(result);
         next();
       }
     });
   } else {
-    res.status(400).send({message:"Not verified"})
-
+    res.status(400).send({ message: "Not verified" });
   }
 };
 
