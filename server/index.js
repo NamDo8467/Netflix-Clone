2d125d5b01ce4e588c67ad03f5715a55d7a5296b
NamDo8467 <huynhnamdo@gmail.com>
2021-08-09 11:06:46 +0700

    make it better

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ mongoose.connect(process.env.DB_CONNECTION_LINK, {
 });
 
 
-const PORT = 5500;
+const PORT = process.env.PORT || 5500;
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
 });
