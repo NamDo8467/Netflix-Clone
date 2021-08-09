e08394cf9aad91d17f9414f764dcd255e6d468f0
NamDo8467 <huynhnamdo@gmail.com>
2021-08-09 20:04:16 +0700

    change cors

--- server/index.js
+++ server/index.js
@@ -10,12 +10,16 @@ require("dotenv").config();
 const app = express();
 // app.use(cors({ origin: 'http://localhost:3000', optionsSuccessStatus: 200 }));
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://my-notflix.netlify.app/"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+let corsOptions = {
+  origin: 'https://my-notflix.netlify.app/',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
-app.use(cors())
+app.use(cors(corsOptions))
 
 app.use(cookieParser());
 
