6ebed8230c90ced5788ae2d5419a273b965a08b4
NamDo8467 <huynhnamdo@gmail.com>
2021-08-09 20:25:05 +0700

    new commit

--- server/index.js
+++ server/index.js
@@ -9,13 +9,13 @@ require("dotenv").config();
 
 const app = express();
 // app.use(cors({ origin: 'http://localhost:3000', optionsSuccessStatus: 200 }));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://my-notflix.netlify.app/"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "https://my-notflix.netlify.app/"); // update to match the domain you will make the request from
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 let corsOptions = {
-  origin: 'https://my-notflix.netlify.app/',
+  origin: 'http://localhost:3000',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
