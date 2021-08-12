e8c72fe6190fb9ff0a25b4746cee6ad8838f2cb9
NamDo8467 <huynhnamdo@gmail.com>
2021-08-12 08:52:38 +0700

    test

--- client/src/Logout/Logout.jsx
+++ client/src/Logout/Logout.jsx
@@ -4,31 +4,33 @@ import axios from "axios";
 import { displayLogout, hideLogout } from "../Helpers/Helpers";
 function Logout(props) {
   const logout = async () => {
-    const result = await axios.get("https://whispering-plains-27657.herokuapp.com/logout", "", {
-      withCredentials: true,
-    });
-    console.log(result.data)
-    if (result.data == "Logged out") {
-      localStorage.removeItem("name")
-      localStorage.removeItem("cookie")
-      document.cookie('cookie=;expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/logout')
-      return null;
-    }
-    
-    
+    const result = await axios.get(
+      "https://whispering-plains-27657.herokuapp.com/logout",
+      "",
+      {
+        withCredentials: true,
+      }
+    );
+    console.log(result.data);
+    localStorage.removeItem("name");
+    localStorage.removeItem("cookie");
+    document.cookie(
+      "cookie=;expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/logout"
+    );
+    return null;
   };
 
   return (
     <div
       className="logout"
-      
+
       // onMouseEnter={displayLogout}
       // onMouseLeave={hideLogout}
     >
       {/* document.cookie.split("=")[1].split("%20").join(" ") */}
       <div className="logout-user-info">
         <img className="logout-user-icon" src={userIcon} alt="user icon" />
-        <p>{localStorage.getItem('name')}</p>
+        <p>{localStorage.getItem("name")}</p>
       </div>
       <div className="logout-link">
         <a href="/" onClick={logout}>
