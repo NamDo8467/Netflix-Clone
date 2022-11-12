cc0390ea7cbeb2acdb0b1aec5d659ff8b5ae035f
NamDo8467 <huynhnamdo@gmail.com>
2022-11-11 23:04:32 -0500

    change error handling method

--- client/src/Movies/Movies.jsx
+++ client/src/Movies/Movies.jsx
@@ -1,60 +1,64 @@
-import React, { useState, useEffect } from "react";
-import { useHistory } from "react-router-dom";
-import MovieContainer from "./MovieContainer/MovieContainer";
-import ActionMovies from "./ActionMovies/ActionMovies";
-import FantasyMovies from "./FantasyMovies/FantasyMovies";
-import RomanceMovies from "./RomanceMovies/RomanceMovies";
-import MovieHead from "./MovieHead/MovieHead";
-import "./Movies.css";
-import "../TvShows_Movies.css";
-import axios from "axios";
+import React, { useState, useEffect } from "react"
+// import { useHistory } from "react-router-dom"
+import MovieContainer from "./MovieContainer/MovieContainer"
+import ActionMovies from "./ActionMovies/ActionMovies"
+import FantasyMovies from "./FantasyMovies/FantasyMovies"
+import RomanceMovies from "./RomanceMovies/RomanceMovies"
+import MovieHead from "./MovieHead/MovieHead"
+import "./Movies.css"
+import "../TvShows_Movies.css"
+import axios from "axios"
 function Movies() {
-  let history = useHistory();
+	// let history = useHistory()
+	// const URL = "https://whispering-plains-27657.herokuapp.com/movies"
+	const URL = "http://localhost:5500/movies"
+	const [isVerified, setIsVerified] = useState("")
 
-  const [isVerified, setIsVerified] = useState("Not verified");
+	useEffect(() => {
+		const source = axios.CancelToken.source()
+		axios
+			.get(URL, {
+				withCredentials: true,
+				cancelToken: source.token
+			})
+			.then(result => {
+				setIsVerified(result.data.message)
+			})
+			.catch(error => {
+				setIsVerified(error.response.data)
+				// console.log(error.response.data)
+				// setIsVerified(error.)
+				// alert(error.message.message)
+				// history.goBack()
+			})
 
-  useEffect(() => {
-    const source = axios.CancelToken.source();
-    axios
-      .get("https://whispering-plains-27657.herokuapp.com/movies", {
-        withCredentials: true,
-        cancelToken: source.token,
-      })
-      .then((result) => {
-        setIsVerified(result.data.message);
-      })
-      .catch((error) => {
-        console.log(error);
-        history.goBack();
-      });
-    if (isVerified == "Not verified") {
-      return () => {
-        source.cancel();
-      };
-    }
-  }, [isVerified, history]);
-  if (isVerified == "Verified") {
-    return (
-      <MovieContainer>
-        <MovieHead />
-        <ActionMovies />
-        <FantasyMovies />
-        <RomanceMovies />
-      </MovieContainer>
-    );
-  } else {
-    return (
-      <div
-        style={{
-          backgroundColor: "white",
-          height: "100vh",
-        }}
-      >
-        <p>Access denied</p>
-        <a href="/login">Login</a>
-      </div>
-    );
-  }
+		return () => {
+			source.cancel()
+		}
+	}, [])
+	if (isVerified === "Verified") {
+		// console.log(isVerified)
+		return (
+			<MovieContainer>
+				<MovieHead />
+				<ActionMovies />
+				<FantasyMovies />
+				<RomanceMovies />
+			</MovieContainer>
+		)
+	} else {
+		return (
+			<div
+				style={{
+					backgroundColor: "white",
+					height: "100vh"
+				}}
+			>
+				<p>Access denied</p>
+				<a href='/login'>Login</a>
+			</div>
+		)
+	}
 }
 
-export default Movies;
+export default Movies
