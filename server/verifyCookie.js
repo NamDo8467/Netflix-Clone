6a6e6bf82fabdedbf08527959d8077830c8e7a07
NamDo8467 <huynhnamdo@gmail.com>
2022-11-11 23:09:38 -0500

    better verified cookie

--- server/verifyCookie.js
+++ server/verifyCookie.js
@@ -1,19 +1,18 @@
-const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken")
 
 const verifyCookie = (req, res, next) => {
-  const cookie = req.cookies.cookie;
-  console.log(cookie)
-  if (cookie) {
-    jwt.verify(cookie, "netflix secret key", (err, result) => {
-      if (err) {
-        res.status(400).send({ message: "Not verified" });
-      } else {
-        next();
-      }
-    });
-  } else {
-    res.status(400).send({ message: "Not verified" });
-  }
-};
+	const cookie = req.cookies.jwtAuth
+	if (cookie) {
+		jwt.verify(cookie, "netflix secret key", (err, result) => {
+			if (err) {
+				res.status(400).send({ message: "Not verified" })
+			} else {
+				next()
+			}
+		})
+	} else {
+		res.status(400).send({ message: "Not verified" })
+	}
+}
 
-module.exports = verifyCookie;
+module.exports = verifyCookie
