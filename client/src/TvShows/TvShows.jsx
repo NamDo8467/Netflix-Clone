36e6098aab71c6927ce7e8478facc0b7baeb0532
NamDo8467 <huynhnamdo@gmail.com>
2022-11-11 23:07:39 -0500

    refactored code

--- client/src/TvShows/TvShows.jsx
+++ client/src/TvShows/TvShows.jsx
@@ -1,62 +1,63 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { useHistory } from "react-router-dom";
-import TvShowContainer from "./TvShowContainer/TvShowContainer";
-import TvShowHead from "./TvShowHead/TvShowHead";
-import PopularTvShows from "./PopularTvShows/PopularTvShows";
-import FamilyTvShows from "./FamilyTvShows/FamilyTvShows";
-import ComedyTvShows from "./ComedyTvShows/ComedyTvShows";
-import "./TvShows.css";
-import "../TvShows_Movies.css";
+import React, { useState, useEffect } from "react"
+import axios from "axios"
+// import { useHistory } from "react-router-dom"
+import TvShowContainer from "./TvShowContainer/TvShowContainer"
+import TvShowHead from "./TvShowHead/TvShowHead"
+import PopularTvShows from "./PopularTvShows/PopularTvShows"
+import FamilyTvShows from "./FamilyTvShows/FamilyTvShows"
+import ComedyTvShows from "./ComedyTvShows/ComedyTvShows"
+import "./TvShows.css"
+import "../TvShows_Movies.css"
 
 function TvShows() {
-  let history = useHistory();
-  const [isVerified, setIsVerified] = useState("Not verified");
+	// let history = useHistory()
+	// const URL = "https://whispering-plains-27657.herokuapp.com/tvshows"
+	const URL = "http://localhost:5500/tvshows"
+	const [isVerified, setIsVerified] = useState("Not verified")
 
-  useEffect(() => {
-    const source = axios.CancelToken.source();
-    axios
-      .get("https://whispering-plains-27657.herokuapp.com/tvshows", {
-        withCredentials: true,
-        cancelToken: source.token,
-      })
-      .then((result) => {
-        setIsVerified(result.data.message);
+	useEffect(() => {
+		const source = axios.CancelToken.source()
 
-      })
-      .catch((error) => {
-        console.log(error);
-        // history.goBack();
-      });
-    if (isVerified == "Not verified") {
-      return () => {
-        source.cancel();
-      };
-    }
-  }, [isVerified, history]);
-  if (isVerified == "Verified") {
-    return (
-      <TvShowContainer>
-        <TvShowHead />
-        <PopularTvShows />
-        <FamilyTvShows />
-        <ComedyTvShows />
-      </TvShowContainer>
-    );
-  } else {
-    return (
-      <div
-        style={{
-          backgroundColor: "white",
-          height: "100vh",
-          marginLeft:"50px"
-        }}
-      >
-        <p>Access denied</p>
-        <a href="/login">Login</a>
-      </div>
-    );
-  }
+		axios
+			.get(URL, {
+				withCredentials: true,
+				cancelToken: source.token
+			})
+			.then(result => {
+				setIsVerified(result.data.message)
+			})
+			.catch(error => {
+				setIsVerified(error.response.data)
+				// history.goBack();
+			})
+
+		return () => {
+			source.cancel()
+		}
+	}, [])
+	if (isVerified === "Verified") {
+		return (
+			<TvShowContainer>
+				<TvShowHead />
+				<PopularTvShows />
+				<FamilyTvShows />
+				<ComedyTvShows />
+			</TvShowContainer>
+		)
+	} else {
+		return (
+			<div
+				style={{
+					backgroundColor: "white",
+					height: "100vh",
+					marginLeft: "50px"
+				}}
+			>
+				<p>Access denied</p>
+				<a href='/login'>Login</a>
+			</div>
+		)
+	}
 }
 
-export default TvShows;
+export default TvShows
