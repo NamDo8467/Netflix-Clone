99cd3a359aad3bb31caf7509844c7b8e33010d3e
NamDo8467 <huynhnamdo@gmail.com>
2022-11-11 23:04:53 -0500

    add link to the logo

--- client/src/NavBar/NavBar.jsx
+++ client/src/NavBar/NavBar.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import './NavBar.css'
-import netflixLogo from '../images/netflix-logo.svg'
+import React from "react"
+import "./NavBar.css"
+import netflixLogo from "../images/netflix-logo.svg"
 
 function NavBar() {
-    return (
-        <div className='navbar-container'>
-            <img className='netflix-logo' src={netflixLogo} alt="netflix logo" />
-            <a className='sign-in-button' href="/login">Sign In</a>
-        </div>
-    )
+	return (
+		<div className='navbar-container'>
+			<a href='/'>
+				{" "}
+				<img className='netflix-logo' src={netflixLogo} alt='netflix logo' />
+			</a>
+			<a className='sign-in-button' href='/login'>
+				Sign In
+			</a>
+		</div>
+	)
 }
 
 export default NavBar
